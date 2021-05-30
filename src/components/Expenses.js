447be3cec1013c3bd587527b216758d995856e82
Rey Richards <rey.richards@omnigroup.com.au>
2021-05-30 15:57:50 +0800

    assignment submission

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,18 +1,20 @@
 import ExpenseItem from './ExpenseItem';
+import './Expenses.css'
 
 function Expenses(props) {
 
     const expenses = props.expenseData;
 
     return (
-        <div>
-            {
-                expenses.map(data =>
+        <div >
+            <div className='expenses'>
+                {
+                    expenses.map(data =>
+                        <ExpenseItem  date={data.date} title={data.title} amount={data.amount} />
 
-                    <ExpenseItem date={data.date} title={data.title} amount={data.amount} />
-
-                )
-            }
+                    )
+                }
+            </div>
         </div>
     )
 
